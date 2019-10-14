64e9e4820017282597393bdeb70d3013b9ac87d3
rlebell33 <rlebell33@gmail.com>
2019-10-14 15:20:38 -0500

    got hello world to work :D

---
+++ main.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        System.exit( 0 ); //success
+    }
+}
+
+// BufferedReader csvReader = new BufferedReader(new FileReader(pathToCsv));
+// while ((row = csvReader.readLine()) != null) {
+//     String[] data = row.split(",");
+//     // do something with the data
+// }
+// system.out.print(data);
+// csvReader.close();
